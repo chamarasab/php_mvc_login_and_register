428a244b18b1dfa9b25bd1bcf97f45ab0ec9cf98
chamarasab <chamara.sab@outlook.com>
2024-07-18 11:14:56 +0530

    bug fix of login response

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -19,6 +19,7 @@ class UserController extends Controller
         return $this->jsonResponse($result);
     }
 
+    /*
     public function login()
     {
         $input = $this->getInput();
@@ -28,6 +29,29 @@ class UserController extends Controller
         if ($user && password_verify($input['password'], $user['password'])) {
             $userModel->updateLoginTime($user['id']);
             return $this->jsonResponse(['message' => 'Login successful', 'user' => $user]);
+        } else {
+            return $this->jsonResponse(['message' => 'Invalid email or password'], 401);
+        }
+    }
+    */
+    public function login()
+    {
+        $input = $this->getInput();
+        $userModel = new UserModel();
+        $user = $userModel->getUserByEmail($input['email']);
+
+        if ($user && password_verify($input['password'], $user['password'])) {
+            $userModel->updateLoginTime($user['id']);
+            
+            // Filter the user data to remove unnecessary indexed elements
+            $filteredUser = [
+                'id' => $user['id'],
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'logged_at' => date('Y-m-d H:i:s')
+            ];
+
+            return $this->jsonResponse(['message' => 'Login successful', 'user' => $filteredUser]);
         } else {
             return $this->jsonResponse(['message' => 'Invalid email or password'], 401);
         }
